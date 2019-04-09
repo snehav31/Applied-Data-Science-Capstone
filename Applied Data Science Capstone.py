af18b26a1ddc49e00154ae28f9dc75f0936e16e5
snehav31 <47804082+snehav31@users.noreply.github.com>
2019-04-09 13:41:03 -0400

    Notebook for Applied Data Science Capstone Course

---
+++ Applied Data Science Capstone.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# # Notebook for Applied Data Science Capstone
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
